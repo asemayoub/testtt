daafb02819ddab0a9947363aec883241dad49923
asem ayoub <asemayoub2011@gmail.com>
2023-01-08 18:01:58 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:testtt/screens/home_page.dart';
+import 'package:testtt/screens/update_product_page.dart';
+
+void main() {
+  runApp(const StoreApp());
+}
+
+class StoreApp extends StatelessWidget {
+  const StoreApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        HomePage.id: (context) => HomePage(),
+        UpdateProductPage.id: (context) => UpdateProductPage(),
+      },
+      initialRoute: HomePage.id,
+    );
+  }
+}
